4317886ac3b0c37768f4c5538d7335303ab1cdca
toxonpf <toxonpf@gmail.com>
2026-05-21 13:01:09 +0500

    add lesson25 html/js; add dark theme to all html files

--- HEXLET/KZ/разработка модулей програмного обеспечения/РО 3.2 Разрабатывать модули с применением DOM API, Regexp, HTTP/lesson25/lesson25.js
+++ HEXLET/KZ/разработка модулей програмного обеспечения/РО 3.2 Разрабатывать модули с применением DOM API, Regexp, HTTP/lesson25/lesson25.js
@@ -1,54 +1,58 @@
-// ЛР-26: DOM. Структура и навигация. Вариант 1
-
-// Задание 1
-function task1() {
-    console.log("Задание 1:", document.body.childNodes);
-
-    document.body.childNodes.forEach((node, i) => {
-        if (node.nodeType === Node.TEXT_NODE) {
-            console.log(`  [${i}] Текстовый узел (перенос строки/пробелы)`);
-        } else if (node.nodeType === Node.ELEMENT_NODE) {
-            console.log(`  [${i}] Элемент: <${node.tagName.toLowerCase()}>`);
-        }
-    });
-}
+// ЛР-25: JavaScript в браузере. Вариант 1
 
 // Задание 2
 function task2() {
-    const ul = document.querySelector("#list2");
-    console.log("Задание 2 firstElementChild:", ul.firstElementChild);
-    console.log("Задание 2 lastElementChild:", ul.lastElementChild);
+    console.log("Задание 2 location.href:", location.href);
+    console.log("Задание 2 navigator.userAgent:", navigator.userAgent);
+    console.log("Задание 2 history.length:", history.length);
 }
 
 // Задание 3
 function task3() {
-    const secondP = document.querySelectorAll(".para")[1];
-    console.log("Задание 3 previousElementSibling:", secondP.previousElementSibling);
-    console.log("Задание 3 nextElementSibling:", secondP.nextElementSibling);
+    const container = document.getElementById("container");
+    console.log("Задание 3 container:", container);
+    console.log("Задание 3 container.childNodes:", container.childNodes);
 }
 
 // Задание 4
 function task4() {
-    const p = document.querySelector("#task4 p");
-    console.log("Задание 4 parentNode:", p.parentNode);
-    console.log("Задание 4 parentElement:", p.parentElement);
-    console.log("Задание 4 равны?", p.parentNode === p.parentElement);
-
-    console.log("Задание 4 documentElement.parentNode:", document.documentElement.parentNode);
-    console.log("Задание 4 documentElement.parentElement:", document.documentElement.parentElement);
+    const container = document.getElementById("container");
+    console.log("Задание 4 childNodes:", container.childNodes);
+    console.log("Задание 4 children:", container.children);
+    console.log("Задание 4 children.length:", container.children.length, "— только <p>");
+    console.log("Задание 4 childNodes.length:", container.childNodes.length, "— текст, элемент, комментарий");
 }
 
 // Задание 5
 function task5() {
-    const ul = document.querySelector("#list5");
-    for (const li of ul.children) {
-        li.style.border = "1px solid red";
-    }
-    console.log("Задание 5: рамка добавлена", ul.children.length, "элементам");
+    console.log("Задание 5: выбери <div id='container'> в панели Elements");
+    console.log("  $0.childNodes — все узлы (текст, элемент, комментарий)");
+    console.log("  $0.children   — только дочерние теги");
 }
 
-task1();
+// Задание 6
+function task6() {
+    console.dir(document);
+    console.log("Задание 6 document.URL:", document.URL);
+    console.log("Задание 6 document.title:", document.title);
+    console.log("Задание 6 document.body:", document.body);
+    document.title = "Новый заголовок";
+    console.log("Задание 6 title изменён на:", document.title);
+}
+
+// Задание 7
+function task7() {
+    console.dir(window);
+    console.dir(document);
+    // window — глобальный объект браузера: содержит alert, location, history, document и др.
+    // document — объект текущей страницы (DOM): содержит URL, title, body, querySelector и др.
+    // document доступен через window.document, т.к. document — свойство объекта window
+    // document.alert("test") → TypeError: document.alert is not a function (alert принадлежит window)
+}
+
 task2();
 task3();
 task4();
 task5();
+task6();
+task7();
